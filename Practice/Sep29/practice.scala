4165abdebf5b3dd22fca51a21f774d046493c0dc
aakashsaini96 <aakashsaini.person@gmail.com>
2023-09-29 17:34:14 +0530

    Added comments to explain the practice code better

--- Practice/Sep29/practice.scala
+++ Practice/Sep29/practice.scala
@@ -2,31 +2,31 @@ import io.StdIn._
 
 object Demo {
   def main(args: Array[String]) {
-    val a = 5.6
+    val a = 5.6        //Working with datatyes and variables
     println(a)
 
-    println(a.toInt)
+    println(a.toInt)             //Basic methods to convert to a datatype
     println(('a'+1).toChar)
     println("a"+1)
 
-    println(53.toBinaryString)
-    println(20000000000L+20000000000L)
+    println(53.toBinaryString)            //Binary number system
+    println(20000000000L+20000000000L)      //Long Int datatype
 
-    val para =
+    val para =                     //Multi line string where any character will be allowed
       """This \is a
         |multi-line \n paragraph.
         |""".stripMargin
     println(para)
-    println(s"The number should be ${a+1.1}")
+    println(s"The number should be ${a+1.1}")   //String interpolation
 
-    val str1 = "hi there"
+    val str1 = "hi there"       //Working with string methods
     println(str1.indexOf("h"))
     val space = str1.indexOf(" ")
     val (first, last) = str1.splitAt(space)
     println(s"${last.trim}, $first")
 
     println("Type something: ")
-    val inp = readLine()
+    val inp = readLine()        //Taking input in scala
     println(inp)
   }
 }
